8259b89272d4e3582af921492ff3f1b2ed65f3d9
mnpandu <57147498+mnpandu@users.noreply.github.com>
2023-11-04 21:29:40 -0400

    updated

--- src/test/java/com/websocket/wstutorial/WsServerApplicationTests.java
+++ src/test/java/com/websocket/wstutorial/WsServerApplicationTests.java
@@ -1,13 +1,6 @@
 package com.websocket.wstutorial;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class WsServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
